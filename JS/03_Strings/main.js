fea9831c32af7ab16e27982ac905cd0720cbd4e6
Lucas-Marquess <lucas.marquesm22@gmail.com>
2022-10-10 21:48:38 -0300

    Arquivos da Aula 03 de JS

---
+++ JS/03_Strings/main.js
@@ -0,0 +1,21 @@
+console.log("Gabriela esta sem a camisa do IOS " + "Mayara esta sem camisa a do IOS " + "Alunos sem cracha")
+
+h = 20
+j = 5
+console.log(h + j)
+
+var nota = 5
+console.log(`Quando a Gabriela esta sem a camisa do IOS eu vou dar nota ${nota}`)
+
+console.clear();
+
+// Acessando um caracter de uma String// String é um conjunto de caracteres.
+
+var string1 = "Falta a Camisa pessoal"
+
+console.log(string1[0])
+console.log(string1.charAt(3))
+
+var string1 = "Heloisa IOS"
+console.log(string1.length)
+
