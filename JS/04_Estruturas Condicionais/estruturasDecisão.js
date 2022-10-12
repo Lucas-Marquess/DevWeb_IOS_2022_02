df16d433c4cd3be964d9f2fe6178706c544018ef
Lucas-Marquess <lucas.marquesm22@gmail.com>
2022-10-11 21:41:43 -0300

    Arquivos da Aula 04 de JS

---
+++ JS/04_Estruturas Condicionais/estruturasDecisão.js
@@ -0,0 +1,20 @@
+// Exercicio 1 
+
+var num1 = 'R$10';
+var num2 = 'R$30';
+var num3 = 'R$5';
+
+if(num1>num2){
+if(num1>num3){
+console.log("R$10 é o menor!");
+}else
+console.log("R$3 é o menor!");
+}else if(num2>num1){
+if(num1>num3){
+console.log("R$50 é o menor!");
+}else
+console.log("R$3 é o menor!");
+}
+
+// Exercicio 2
+
