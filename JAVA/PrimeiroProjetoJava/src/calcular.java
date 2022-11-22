6bef5e63690d804b6a1af329ef74fb0cc6c31c6c
Lucas-Marquess <lucas.marquesm22@gmail.com>
2022-11-21 21:53:23 -0300

    Aula de Java 01

---
+++ JAVA/PrimeiroProjetoJava/src/calcular.java
@@ -0,0 +1,11 @@
+public class calcular {
+    // primeiro passo criar metodo main
+    public static void main(String[] args){
+        // Vamos somar a idade de dois alunos
+        // var idade1 = 25
+        int idade1 = 25;
+        int idade2 = 15;
+        int somaIdade = idade1 + idade2;
+        System.out.println("A soma da idade dos alunos é " + somaIdade);
+    }
+}
---
+++ JAVA/PrimeiroProjetoJava/src/pipoca.java
@@ -0,0 +1,11 @@
+public class pipoca {
+    // Mostrar no console a frase hello World
+    public static void main(String[] args){
+            String texto = "Hello World";
+            System.out.println(texto);
+            System.out.println(45);
+        //console.log("Hello World")
+    }
+}
+
+
