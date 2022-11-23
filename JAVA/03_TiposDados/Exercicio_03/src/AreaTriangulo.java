d6461da19f4a281a63df99b67f6da48dd7fbd4ea
Lucas-Marquess <lucas.marquesm22@gmail.com>
2022-11-22 21:24:03 -0300

    Exercico da Aula 03 de Java

---
+++ JAVA/03_TiposDados/Exercicio_03/src/AreaTriangulo.java
@@ -0,0 +1,9 @@
+public class AreaTriangulo {
+    public static void main(String[] args) {
+        int base = 8;
+        int altura = 4;
+        int area = (base * altura) / 2 ;
+
+        System.out.println("A Área desse triângulo é: " + area);
+    }
+}
